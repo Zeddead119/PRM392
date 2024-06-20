23ba9430a1c9b7a85d3ba6c4750bb53ec0c19f94
Ngo Hai Thy <91452361+Zeddead119@users.noreply.github.com>
2024-06-21 02:10:01 +0700

    Slot14 : Fragment, GoogleMap
    Exercise 19 : Fragment
    Google Exercise

---
+++ Exercise19_Fragment/app/src/main/java/com/example/exercise19_fragment/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.exercise19_fragment;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        String param1 = "First";
+        String param2 = "Second";
+        String param3 = "Fragment";
+
+        FirstFragment firstFragment = FirstFragment.newInstance(param1, param3);
+        FirstFragment secondFragment = FirstFragment.newInstance(param2, param3);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container1, firstFragment);
+        fragmentTransaction.replace(R.id.fragment_container2, secondFragment);
+        fragmentTransaction.commit();
+
+    }
+}
---
+++ GoogleMapExercise/app/src/main/java/com/example/googlemapexercise/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.googlemapexercise;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.frmMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(20.866389, 106.6825);
+        googleMap.addMarker(new MarkerOptions().position(location).title("Hai Phong"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+    }
+
+}
